df49a96f4f81fb89596a93a37d0e1de028b4f59b
KawamuraHaruka <itleigns@gmail.com>
2016-11-24 12:11:36 +0900

    バイナリ化

---
+++ assembler3.c
@@ -0,0 +1,19 @@
+void execute(char* s){
+	int i,j;
+	for(int i=0;i<4;i++){
+		int count = 0;
+		for(int j=0;j<8;j++){
+			if(s[i*8+j] == '1'){
+				count |= 1 << (7-j);
+			}
+		}
+		putchar(count);
+	}
+}
+int main(int argc,char *argv[]){
+	char s[32];
+	while(gets(s) != NULL){
+		execute(s);
+	}
+	return 0;
+}
